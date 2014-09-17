c85f373a106661e0181f53de06f8134630e836e5
henarons <henrik.brautaset.aronsen@finn.no>
2014-09-17 12:14:35 +0200

    Add all tests (maps/lists/null/escaping/hashcode)

--- src/main/java/JsonEncoder.java
+++ src/main/java/JsonEncoder.java
@@ -1,26 +1,71 @@
+import java.util.AbstractList;
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.hash.HashCode;
 import groovy.lang.GString;
 import org.boon.json.JsonSerializer;
 import org.boon.json.JsonSerializerFactory;
 import org.boon.json.serializers.JsonSerializerInternal;
 import org.boon.json.serializers.impl.AbstractCustomObjectSerializer;
 import org.boon.primitive.CharBuf;
-import org.codehaus.groovy.runtime.GStringImpl;
 
 public final class JsonEncoder {
 
     private JsonEncoder() {}
 
     public static String toJson(Object obj) {
-        JsonSerializerFactory jsonSerializerFactory = new JsonSerializerFactory()
-                .addTypeSerializer(GStringImpl.class, new AbstractCustomObjectSerializer(GStringImpl.class) {
+        return createJsonSerializer().serialize(obj).toString();
+    }
 
-                    @Override
-                    public void serializeObject(JsonSerializerInternal serializer, Object instance, CharBuf builder) {
-                        builder.addString(((GString) instance).toString());
-                    }
-                });
-        JsonSerializer serializer = jsonSerializerFactory.create();
-        return serializer.serialize(obj).toString();
+    private static JsonSerializer createJsonSerializer() {
+        JsonSerializerFactory jsonSerializerFactory = new JsonSerializerFactory()
+                .includeNulls()
+                .setEncodeStrings(true)
+                .addTypeSerializer(GString.class, new StringSerializer())
+                .addTypeSerializer(AbstractList.class, new ListSerializer())  // e.g. groovyx.gpars.extra166y.ParallelArray$AsList
+                .addTypeSerializer(AbstractMap.class, new MapSerializer())    // e.g. groovy.json.internal.LazyMap
+                .addTypeSerializer(ImmutableMap.class, new MapSerializer())  // e.g. com.google.common.collect.RegularImmutableMap
+                .addTypeSerializer(HashCode.class, new StringSerializer());
+        return jsonSerializerFactory.create();
+    }
+
+    private static final class MapSerializer extends AbstractCustomObjectSerializer<Map> {
+
+        public MapSerializer() {
+            super(Map.class);
+        }
+
+        @Override
+        public void serializeObject(JsonSerializerInternal serializer, Map instance, CharBuf builder) {
+            serializer.serializeMap(instance, builder);
+        }
+    }
+
+    private static final class ListSerializer extends AbstractCustomObjectSerializer<Collection> {
+
+        public ListSerializer() {
+            super(Collection.class);
+        }
+
+        @Override
+        public void serializeObject(JsonSerializerInternal serializer, Collection instance, CharBuf builder) {
+            serializer.serializeCollection(instance, builder);
+        }
+    }
+
+    private static final class StringSerializer extends AbstractCustomObjectSerializer<Object> {
+
+        public StringSerializer() {
+            super(Object.class);
+        }
+
+        @Override
+        public void serializeObject(JsonSerializerInternal serializer, Object instance, CharBuf builder) {
+            serializer.serializeString(instance.toString(), builder);
+        }
     }
 }
 
--- src/test/groovy/JsonEncoderTest.groovy
+++ src/test/groovy/JsonEncoderTest.groovy
@@ -1,13 +1,107 @@
+import java.nio.charset.Charset
+
+import com.google.common.collect.ImmutableMap
+import com.google.common.hash.HashCode
+import com.google.common.hash.Hasher
+import groovy.json.internal.LazyMap
+import groovyx.gpars.extra166y.ParallelArray
 import spock.lang.Specification
+
+import static JsonEncoder.toJson
+import static com.google.common.hash.Hashing.md5
 
 class JsonEncoderTest extends Specification {
 
     def "should serialize GString correctly"() {
-      when:
+      given:
         String value = 'hello'
-        String actualJson = JsonEncoder.toJson([key: "AGString $value"])
+        def map = [key: "AGString $value"]
+
+      when:
+        String actualJson = toJson(map)
 
       then:
+        map.key.getClass().simpleName == "GStringImpl"
         '{"key":"AGString hello"}' == actualJson
+    }
+
+    def "should serialize groovy LazyMap correctly"() {
+      given:
+        LazyMap map = new LazyMap()
+        map.putAll([
+            somekey: "somevalue",
+            otherKey: [
+                "otherValue",
+                "thirdValue"
+            ]
+        ])
+
+      when:
+        String result = toJson(map)
+
+      then:
+        map.getClass().simpleName == "LazyMap"
+        result == '{"otherKey":["otherValue","thirdValue"],"somekey":"somevalue"}'
+    }
+
+    def "should serialize guava ImmutableMap"() {
+      given:
+        def map = ImmutableMap.of("key", "ho", "key2", "pong")
+
+      when:
+        String result = toJson(map)
+
+      then:
+        map.getClass().simpleName == "RegularImmutableMap"
+        result == '{"key":"ho","key2":"pong"}'
+    }
+
+    def "should serialize gpars ParallelArray.AsList"() {
+      given:
+        def list = ["hei", "sann", "du"]
+        def aslist = ParallelArray.createFromCopy(list.size(), list.toArray(), ParallelArray.defaultExecutor()).asList()
+
+      when:
+        String result = toJson(aslist)
+
+      then:
+        aslist.getClass().simpleName == "AsList"
+        result == '["hei","sann","du"]'
+    }
+
+    def "should serialize maps with null values"() {
+      given:
+        Map map = [ key: "sometext", nullkey: null ]
+
+      when:
+        String result = toJson(map)
+
+      then:
+        result == '{"key":"sometext","nullkey":null}'
+    }
+
+    def "should encode non-ascii values"() {
+      given:
+        String stuff = "heiæøå"
+
+      when:
+        String result = toJson(stuff)
+
+      then:
+        result == '"hei\\\\u00E6\\\\u00F8\\\\u00E5"'
+    }
+
+    def "should serialize HashCode correctly"() {
+      given:
+        Hasher hasher = md5().newHasher()
+        hasher.putString("heisann", Charset.defaultCharset())
+        HashCode hash = hasher.hash()
+
+      when:
+        String actualJson = toJson(hash)
+
+      then:
+        hash.getClass().simpleName == "BytesHashCode"
+        actualJson == '"86c7c929d73e1d91c268c9f18d121212"'
     }
 }
